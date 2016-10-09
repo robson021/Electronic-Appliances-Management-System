6637f0fea29c8633daa9544f5efc5ba02c902f18
robert <nowak021@gmail.com>
2016-10-09 19:30:04 +0200

    1st commit

---
+++ ElectronicAppliancesManagementSystemCore/src/main/java/robert/ElectronicAppliancesManagementSystemCoreApplication.java
@@ -0,0 +1,12 @@
+package robert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicAppliancesManagementSystemCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicAppliancesManagementSystemCoreApplication.class, args);
+	}
+}
