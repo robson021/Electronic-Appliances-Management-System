1042937d4448de0b20c678ab501e3c8b8e789b62
robert <nowak021@gmail.com>
2016-10-09 19:50:06 +0200

    Replace Tomcat with Jetty. Add test REST service

---
+++ ElectronicAppliancesManagementSystemCore/src/main/java/robert/svc/TestController.java
@@ -0,0 +1,13 @@
+package robert.svc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String hello() {
+        return "Hello World";
+    }
+}
