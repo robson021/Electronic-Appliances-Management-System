966f292bb3aef9dd38fe6694da3a3feb6c655ab0
robert <nowak021@gmail.com>
2016-10-10 15:10:48 +0200

    create simple MySql connection

--- ElectronicAppliancesManagementSystemCore/src/main/java/robert/ElectronicAppliancesManagementSystemCoreApplication.java
+++ ElectronicAppliancesManagementSystemCore/src/main/java/robert/ElectronicAppliancesManagementSystemCoreApplication.java
@@ -1,9 +1,15 @@
 package robert;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EntityScan(basePackages = "robert.db.entity")
+@EnableJpaRepositories
 public class ElectronicAppliancesManagementSystemCoreApplication {
 
 	public static void main(String[] args) {
--- ElectronicAppliancesManagementSystemCore/src/main/java/robert/svc/TestController.java
+++ ElectronicAppliancesManagementSystemCore/src/main/java/robert/svc/TestController.java
@@ -3,11 +3,13 @@ package robert.svc;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class TestController {
 
     @RequestMapping("/test")
     public String hello() {
-        return "Hello World";
+        return "Hello World " + new Date().toString();
     }
 }
