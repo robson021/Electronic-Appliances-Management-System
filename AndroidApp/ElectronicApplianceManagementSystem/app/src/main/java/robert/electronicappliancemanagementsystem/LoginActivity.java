d8df87efa164097871355644b773ad604f252baf
robert <nowak021@gmail.com>
2016-11-29 20:59:44 +0100

    add button listenere

--- AndroidApp/ElectronicApplianceManagementSystem/app/src/main/java/robert/electronicappliancemanagementsystem/LoginActivity.java
+++ AndroidApp/ElectronicApplianceManagementSystem/app/src/main/java/robert/electronicappliancemanagementsystem/LoginActivity.java
@@ -1,7 +1,10 @@
 package robert.electronicappliancemanagementsystem;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
 
@@ -9,5 +12,16 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new RegisterButtonAction());
+    }
+
+    private class RegisterButtonAction implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            LoginActivity.this.startActivity(intent);
+        }
     }
 }
