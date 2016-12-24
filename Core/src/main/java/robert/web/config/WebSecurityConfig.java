41552c655fb49c04ec6c3e2f9ef681df1a6f4cde
robert <nowak021@gmail.com>
2016-12-24 16:08:20 +0100

    change session creation policy

--- Core/src/main/java/robert/web/config/WebSecurityConfig.java
+++ Core/src/main/java/robert/web/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +20,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.permitAll();
 		http.csrf()
 				.disable();
+
+		http.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.NEVER)
+				.and()
+				.httpBasic();
 	}
 
 }
