05a2137dc0ee91bdb34bd65f9ed32670e76f16be
robert <nowak021@gmail.com>
2017-01-08 16:11:26 +0100

    Validation cleanup

--- Core/src/main/java/robert/enums/Validation.java
+++ Core/src/main/java/robert/enums/Validation.java
@@ -1,7 +1,6 @@
 package robert.enums;
 
-import org.assertj.core.util.Lists;
-
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -18,12 +17,12 @@ public interface Validation {
 
 	String ADMIN_URI = ADMIN_PREFIX + "**";
 
-	List<String> NO_AUTH_URIS = Collections.unmodifiableList(    // TODO: replace with Guava?
-			Lists.newArrayList("/", "/login/**", "/register/**", "/logout/**", "/guest/**", "/test/**")
+	List<String> NO_AUTH_URIS = Collections.unmodifiableList(
+			Arrays.asList("/", "/login/**", "/register/**", "/logout/**", "/guest/**", "/test/**")
 	);
 
 	List<String> NO_AUTH_FILES = Collections.unmodifiableList(
-			Lists.newArrayList(".html", ".js", ".css")
+			Arrays.asList(".html", ".js", ".css")
 	);
 
 	short MAX_RESERVATION_TIME_IN_MINUTES = 12 * 60;
